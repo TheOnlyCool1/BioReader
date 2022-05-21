40119383464ced725fd5c5560632f55c3b308e9c
TheCool <53025128+TheOnlyCool1@users.noreply.github.com>
2022-05-20 23:41:35 -0700

    Prepared to give up(?)
    Cleaned up and added comments if anyone wants to try and make this happen

--- bolder.js
+++ bolder.js
@@ -1,29 +1,37 @@
-
-
-chrome.webNavigation.onCompleted.addListener(function (ta) {
-    let [tab] = chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: bionicPage
-    });
+chrome.action.onClicked.addListener((tab) => {
+    if (!tab.url.includes("chrome://")) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id, allFrames: true },
+            function: bionicPage
+        });
+    }
 });
 
 function bionicPage() {
-    /*var div = document.getElementsByTagName("div");
-    var sp = document.getElementsByTagName("span");
+    let div = document.getElementsByTagName("p");
+    //Maybe search with more tags like "div" and not just "p"
 
+    //Loops through all elements
     for (var i = 0; i < div.length; i++) {
-        div[i].style.fontWeight = "bolder";
+        //div[i].style.fontWeight = "bolder"; How to make the font bold, only works for one element (unmodified means a whole text block)
+
+        //Attempts to get the text, although idk if it actually works since chrome console.log doesn't work with scripting.executeScript()
+        let text = div[i].textContent;
+        //Splits text into words
+        let split = text.split(" ");
+        //let parent = div[i].parentElement;
+
+        //Tries to break text blocks into words, currently don't know where or how to place the nodes.
+        for (var j = 0; j < split.length; j++) {
+            //Creates node
+            let node = document.createTextNode(split[j]);
+            //Adds the node (to the wrong place as of now)
+            div[i].appendChild(node);
+
+            //Should divide the words in two and embolden them, rounding up.
+            //Not implemented since I can't even get normal text to work, but once that's up it should be simple.
+        }
+        //Removes old text block once done
+        div[i].remove();
     }
-
-    for (var i = 0; i < sp.length; i++) {
-        [i].style.fontWeight = "bolder";
-    }*/
-
-    let text = document.getElementsByTagName("p");
-    for (var i = 0; i < text.length; i++) {
-        console.log(text[i]);
-
-	}
 }
