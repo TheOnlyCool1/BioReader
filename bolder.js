0cc11085783ad447991c4e696a8d65c5215f7fe6
Me <53025128+TheOnlyCool1@users.noreply.github.com>
2022-05-20 17:39:54 -0700

    Add files via upload

---
+++ bolder.js
@@ -0,0 +1,23 @@
+
+
+chrome.webNavigation.onCompleted.addListener(function (ta) {
+    let [tab] = chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: bionicPage
+  });
+});
+
+function bionicPage() {
+    var div = document.getElementsByTagName("div");
+    var sp = document.getElementsByTagName("span");
+
+    for (var i = 0; i < div.length; i++) {
+        div[i].style.fontWeight = "bolder";
+    }
+
+    for (var i = 0; i < sp.length; i++) {
+        [i].style.fontWeight = "bolder";
+    }
+}
