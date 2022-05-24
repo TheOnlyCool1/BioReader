f087dca9e4afec7f9a26de4b17206af505e77cd6
TheCool <53025128+TheOnlyCool1@users.noreply.github.com>
2022-05-24 01:26:25 -0700

    Got it to work! Sort of...
    It now works with plain text and some very specific formatting. I tried to add some tolerance for HTML labels but depending on the site that just makes it worse.

--- bioreader.js
+++ bioreader.js
@@ -23,18 +23,19 @@ function bionicPage() {
         // Separating & fixing HTML elements
         for(var j = 0; j < split.length; j++) {
             // Checks for HTML tag with space in it
-            if (split[j].indexOf("<") == -1 && split[j].indexOf(">") >= 0) {
+            if (split[j].indexOf("<") >= 0 && split[j].indexOf(">") == -1) {
                 // Merges the two halves of the tag
-                split.splice(j-1,2,split[j-1]+split[j]);
+                split.splice(j,2,split[j]+" "+split[j+1]);
                 j--;
             }
+        }
 
+        for (var j = 0; j < split.length; j++) {
             // Separates HTML tag without space at the end
-            if (split[j].charAt(0) == '<') {
-
+            if (split[j].indexOf("<") >= 0) {
                 let end = split[j].indexOf(">");
-                let itemHTML = split[j].substring(0,end+1);
-                let itemText = "<b>" + split[j].slice(end+1) + "</b>"
+                let itemHTML = split[j].substring(0,end);
+                let itemText = split[j].slice(end+1);
 
                 // Replaces the current element with two elements
                 // First is the HTML part, second is the text part of the word.
@@ -47,10 +48,13 @@ function bionicPage() {
 
         // Inserting bold tags
         for(var j = 0; j < split.length; j++) {
-
+            let word = split[j];
+            if (word.charAt(0) != '<') {
+                split[j] = "<strong>" + word.substring(0,Math.ceil(word.length/2)) + "</strong>" + word.substring(Math.ceil(word.length/2));
+            }
         }
 
-        // Assembling result
+        // Assembling result, merge array into single string
         let result = "";
         split.forEach((value, index, array) => {result += value + " "});
 
