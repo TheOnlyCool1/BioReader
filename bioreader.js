01413e45880e48497bee377d0baf1ec0fafad1d6
TheCool <53025128+TheOnlyCool1@users.noreply.github.com>
2022-05-24 01:34:23 -0700

    Update bioreader.js
    minor tweaks, no major issues solved.

--- bioreader.js
+++ bioreader.js
@@ -31,10 +31,10 @@ function bionicPage() {
         }
 
         for (var j = 0; j < split.length; j++) {
-            // Separates HTML tag without space at the end
+            // Separates HTML tags from words, currently not perfect at all
             if (split[j].indexOf("<") >= 0) {
                 let end = split[j].indexOf(">");
-                let itemHTML = split[j].substring(0,end);
+                let itemHTML = split[j].substring(0,end+1);
                 let itemText = split[j].slice(end+1);
 
                 // Replaces the current element with two elements
@@ -59,35 +59,5 @@ function bionicPage() {
         split.forEach((value, index, array) => {result += value + " "});
 
         p[i].innerHTML = result;
-    }
-}
-
-// Old version, from prior to abandonment
-function bionicPageOld() {
-    let p = document.getElementsByTagName("p");
-    // Maybe search with more tags like "div" and not just "p"
-
-    // Loops through all elements
-    for (var i = 0; i < p.length; i++) {
-        //div[i].style.fontWeight = "bolder"; How to make the font bold, only works for one element (unmodified means a whole text block)
-
-        // Attempts to get the text, although idk if it actually works since chrome console.log doesn't work with scripting.executeScript()
-        let text = p[i].textContent;
-        // Splits text into words
-        let split = text.split(" ");
-        //let parent = p[i].parentElement;
-
-        // Tries to break text blocks into words, currently don't know where or how to place the nodes.
-        for (var j = 0; j < split.length; j++) {
-            // Creates node
-            let node = document.createTextNode(split[j]);
-            // Adds the node (to the wrong place as of now)
-            div[i].appendChild(node);
-
-            // Should divide the words in two and embolden them, rounding up.
-            // Not implemented since I can't even get normal text to work, but once that's up it should be simple.
-        }
-        // Removes old text block once done
-        div[i].remove();
     }
 }
